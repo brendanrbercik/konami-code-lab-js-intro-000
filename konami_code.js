8becdbae8b2f3f03694ecfb7a9c1a916b761ee60
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 00:22:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,19 +15,20 @@ let index = 0;
 
 function init() {
 
-  document.body.addEventListener("keydown", (event) => {
-    const key = e.key;
+}
 
-    if (key === codes[index]) {
-      index++;
+document.body.addEventListener("keydown", (event) => {
+  const key = e.key;
 
-      if (index === codes.length) {
-        alert("KONAMI CODE ENTERED!");
-        index = 0;
-        }
-    }
-      else {
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("KONAMI CODE ENTERED!");
       index = 0;
       }
+  }
+    else {
+    index = 0;
     }
-}
+  }
